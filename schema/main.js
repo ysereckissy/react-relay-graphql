c56701c35c06c3ed21e05230d0bbeb1427616bd8
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2020-12-12 11:19:19 +0100

    rolling dice

--- schema/main.js
+++ schema/main.js
@@ -2,14 +2,21 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
+  GraphQLInt,
 } = require('graphql');
 
+const roll = () => Math.floor(6*Math.random()) + 1;
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
     hello: {
       type: GraphQLString,
       resolve: () => 'World'
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => [roll(), roll()],
     }
   }
 });

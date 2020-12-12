972a17093725b4b419d4e0047b78271d087f7613
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2020-12-12 14:10:47 +0100

    setting an http interface

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ const { graphql } = require('graphql');
 const readline = require('readline');
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
+const { graphqlHTTP } = require('express-graphql');
+const express = require('express');
 
 const exampleSchema = require('./schema/main');
+
+App = express();
 
 const DB_URL = 'mongodb://localhost:27017';
 MongoClient.connect(DB_URL, (err, client) => {
   assert.equal(null, err);
   console.log('Connected to MongoDB server');
+  const db = client.db('test');
+  App.use('/graphql', graphqlHTTP({
+    schema: exampleSchema,
+    context: { db }
+  }));
+  App.listen(5000, () => console.log('Running Express.js on port 5000'));
   /// the readline interface code goes here!
   const rli = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-
   rli.question('Client Request: ', inputQuery => {
-    const db = client.db('test');
-    graphql(exampleSchema, inputQuery, {}, {db}).then(result => {
+    graphql(exampleSchema, inputQuery, {}, { db }).then(result => {
       console.log('Server Answer: ', result.data);
       client.close(() => rli.close());
     });

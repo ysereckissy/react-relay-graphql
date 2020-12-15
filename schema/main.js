4c3714bfa759d19122abb6e786b042a0b9be3c9c
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2020-12-15 19:42:03 +0100

    third chapter the resolve function

--- schema/main.js
+++ schema/main.js
@@ -4,17 +4,75 @@ const {
   GraphQLString,
   GraphQLList,
   GraphQLInt,
+  GraphQLBoolean,
+  GraphQLEnumType,
 } = require('graphql');
 
 const roll = () => Math.floor(6*Math.random()) + 1;
+const toTitleCase = str => {
+  return str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+};
+const LetterCaseType = new GraphQLEnumType({
+  name: 'LetterCase',
+  values: {
+    TITLE: {value: 'title'},
+    UPPER: {value: 'upper'},
+    LOWER: {value: 'lower'}
+  
+}});
+const EmployeeType = new GraphQLObjectType({
+  name: 'Employee',
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      args: {
+        upperCase: {
+          type: GraphQLBoolean,
+          defaultValue: false,
+        }
+      },
+      resolve: (obj, { upperCase }) => {
+        const fullName = `${obj.firstName} ${obj.lastName}`;
+        return upperCase ? fullName.toUpperCase() : fullName;
+      },
+    },
+    nameForCase: {
+      type: GraphQLString,
+      args: {
+        letterCase: {type: LetterCaseType}
+      },
+      resolve: (obj, args) => {
+        const fullName = `${obj.firstName} ${obj.lastName}`;
+        switch(args.letterCase) {
+          case 'lower':
+            return fullName.toLowerCase();
+          case 'upper':
+            return fullName.toUpperCase();
+          case 'title':
+            return toTitleCase(fullName);
+          default:
+            return fullName;
+        }
+      }
+    },
+    boss: {
+      type: EmployeeType
+    }
+  })
+});
+
+const exampleEmployee = {
+  firstName: 'jane',
+  lastName: 'Doe'
+}
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
     hello: {
       type: GraphQLString,
-      resolve: () => 'World'
-    },
+      resolve: () => 'World' },
     diceRoll: {
+      description: '**Simulate** a dice roll determined by count',
       type: new GraphQLList(GraphQLInt),
       args: {
         count: {
@@ -25,8 +83,13 @@ const queryType = new GraphQLObjectType({
       resolve: (_, args) => new Array(args.count).fill(args.count).map((_) => roll()),
     },
     usersCount: {
+      description: 'Total number of users in the database',
       type: GraphQLInt,
       resolve: (_, args, { db }) => db.collection('users').count()
+    },
+    exampleEmployee: {
+      type: EmployeeType,
+      resolve: () => exampleEmployee
     }
   }
 });
